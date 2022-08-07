f56ab52cd2062b9f5ce584a621991753a7657439
Lowry02 <lorenzocusin@MacBook-Air-di-Lorenzo.local>
2022-08-08 00:18:37 +0200

    removing url param

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   //host: env('HOST','0.0.0.0'), 
   host: env('HOST', '185.78.64.49'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://provasito.site'),
+  //url: env('PUBLIC_URL', 'https://provasito.site'),
   app: {
     keys: env.array('APP_KEYS'),
   },
