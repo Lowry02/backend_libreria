5b5bc08805d783d95ecf28e5411b2abddbd3e55a
Lowry02 <lorenzocusin@MacBook-Air-di-Lorenzo.local>
2022-08-07 17:51:12 +0200

    changing url on cinfig server file

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  // host: env('HOST','0.0.0.0'), 
+  //host: env('HOST','0.0.0.0'), 
   host: env('HOST', '185.78.64.49'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://provasito.site'),
   app: {
     keys: env.array('APP_KEYS'),
   },
